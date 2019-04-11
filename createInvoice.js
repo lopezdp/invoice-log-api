399587a2525a17e4d419c22d453516ebacc00fc6
LopezDP <davidplopez@live.com>
2019-04-11 13:36:04 -0400

    docs: Documenting imports needed for createInvoice.js lambda and how to discuss in tutorial.

--- createInvoice.js
+++ createInvoice.js
@@ -6,10 +6,17 @@
  *
  */
 
+// Step1: Discuss the uuid import and need for uuid
+import uuid from "uuid";
+// Step2: Discuss in tutorial why /lib and response-lib
+// exist and why they are used.
 import { success, failure } from "./libs/response-lib";
 
+// Step3: Discuss in tutorial the need for a Dynamo table
+// and a dynamo table implemented for each microservice
+// Implement service below
+//import * as dynamoDbLib from "./libs/dynamodb-lib";
+
 export async function main(event, context) {
-
-
 
 }
