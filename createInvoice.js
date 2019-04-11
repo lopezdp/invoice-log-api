0c289664d396ec42d1f0b9d715ee167ccc3e4e5d
LopezDP <davidplopez@live.com>
2019-04-11 14:05:09 -0400

    feat: Adding to createInvoice.js

--- createInvoice.js
+++ createInvoice.js
@@ -18,5 +18,48 @@ import { success, failure } from "./libs/response-lib";
 //import * as dynamoDbLib from "./libs/dynamodb-lib";
 
 export async function main(event, context) {
+  // The request body is passed in as a JSON string
+  // in event.body and it needs to be parsed!
+  const data = JSON.parse(event.body);
+
+  // Now you need to store the information into an
+  // object that you will use later to store the data
+  // collected from your user into a database of invoices!
+  const params = {
+    // Step4: Need to describe in tutorial how this object is
+    // accessing the table defined in the serverless.yml
+    // and how data is being stored in db.
+    TableName: process.env.invoiceTbl,
+    Item: {
+      // Step5: Discussion on DynamoDB sort key design and
+      // how to properly aggregate data in table to
+      // minimize READS (RCU) from cloud to minimize cost!!!
+      userId: event.requestContext.identity.cognitoIdentityId,
+      invoiceId: uuid.v1(),
+      createdAt: Date.now(),
+      description: data.description,
+      amount: data.amount,
+      attachment: data.attachment
+
+    }
+  };
+
+  try {
+    // async call to db??????
+    // need to implement and test!!!!
+    await test.call();
+    return success(params.Item);
+  } catch ( err ) {
+    return failure({
+      status: false
+    });
+  }
 
 }
+
+
+
+
+
+
+
