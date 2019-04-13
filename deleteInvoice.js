c9fbcbaba8a57e63af80f5c21c5a4bfd1868902e
LopezDP <davidplopez@live.com>
2019-04-13 14:41:21 -0400

    feat: Add deleteInvoice.js to project.

---
+++ deleteInvoice.js
@@ -0,0 +1,13 @@
+/*
+ * Title: deleteInvoice.js Serverless Lambda
+ * Unique Software Development, LLC
+ * Author: David P. Lopez
+ * April 13, 2019
+ *
+ */
+
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+
+}
