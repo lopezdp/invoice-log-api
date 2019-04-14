acd318177e653df3eab4201a7f6745957e5ead5a
LopezDP <davidplopez@live.com>
2019-04-14 16:34:30 -0400

    feat: Completed listInvoices.js with cognito user mapping.

--- listInvoices.js
+++ listInvoices.js
@@ -12,6 +12,19 @@ export async function main(event, context) {
 
   const params = {
     TableName: process.env.tableName,
+    /*
+     * KeyConditionExpression will define the condition for the
+     * query.
+     * - 'userId = :userId' will only return the Items tht match
+     *          the 'userId' PartitionKey.
+     *
+     * ExpressionAttributeValues will define the value in the
+     * condition.
+     * - ':userId': will define a 'userId' that maps to the
+     *           Identity Pool Cognito Identity Id of the
+     *           authenticated user!!!
+     *
+     */
 
     KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
